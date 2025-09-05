f1af16d65ea16d27fac37bd79d4296d090ea977a
Juan Schar <juanschar@gmail.com>
2025-09-05 08:56:31 -0300

    ADD: Variables de entorno

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 Modulo de Aviso de Consultas.
 
 Levantar el proyecto:
-`docker compose build`
-
-Instalar dependencias: `docker exec -it php-app composer install`
+`docker compose -f development.yml build`
+`docker compose -f development.yml run --rm web composer install`
--- bd/conexion.php
+++ bd/conexion.php
@@ -1,10 +1,10 @@
 <?php
 class Conexion {
   public static function Conectar() {
-    define('servidor', 'db'); // nombre del servicio en docker-compose.yml
-    define('nombre_bd', 'gestion_consultas');
-    define('usuario', 'userdb');
-    define('password', 'userdbpass');
+    define('servidor', $_ENV['MYSQL_HOST'] ?? 'db');
+    define('nombre_bd', $_ENV['MYSQL_DATABASE'] ?? 'gestion_consultas');
+    define('usuario', $_ENV['MYSQL_USER'] ?? 'root');
+    define('password', $_ENV['MYSQL_PASSWORD'] ?? 'secret');
     $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8', PDO::ATTR_PERSISTENT => true);
     try {
       $conexion = new PDO("mysql:host=".servidor.";dbname=".nombre_bd, usuario, password, $opciones);
